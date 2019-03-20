f68bd581d6499d65e354ccfc60e921619089fc57
emily-christensen <emilychristensen@mymail.mines.edu>
2019-03-19 20:30:56 -0600

    Memory structure blocked out in comments for EEPROM test code

---
+++ EEPROMTestCode/EEPROMTestCode.ino
@@ -0,0 +1,17 @@
+/*
+ * Write to memory test
+ * This code stores in EEPROM (we have 4Kb of ROM to write to)
+ * Byte 0-3: RFID 1
+ * Bytes 4-31: Reserved for any misc system data needed
+ * Bytes 32+: user data for user 1
+ */
+
+#include <EEPROM.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
