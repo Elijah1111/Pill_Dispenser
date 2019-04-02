717b38cb549a136131a13bda33a5462790311f15
Elijah <elijahmtcastle@gmail.com>
2019-04-02 16:35:52 -0600

    Update Dispenser
    Dispenser now can reconize diffrent id's

--- Dispensing/Dispensing.ino
+++ Dispensing/Dispensing.ino
@@ -4,25 +4,14 @@
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          10         // Configurable, see typical pin layout above
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-bool test(const char input[4]){
-  char master[] = ")⸮V⸮";
-  for(int i=0; i<4; i++){
-    char tmp = input[i];
-    Serial.println(tmp);
-    if(master[i] != tmp){
-      Serial.println("FAILED ON: " + tmp);
-      return false;
-    }
-  }
-  return true;
-}
+MFRC522 reader(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init();   // Init MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  reader.PCD_Init();   // Init MFRC522
+  reader.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
   pinMode(7,OUTPUT);
   pinMode(6,OUTPUT);
@@ -30,30 +19,36 @@ void setup() {
 
 void loop() {
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if ( ! reader.PICC_IsNewCardPresent()) {
     return;
   }
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if ( ! reader.PICC_ReadCardSerial()) {
     return;
   }
   // Dump debug info about the card; PICC_HaltA() is automatically called
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  //reader.PICC_DumpToSerial(&(reader.uid));
 
-  char input[4];
+  byte input[4];
   for(int i =0; i<4; i++){
-    char tmp =char(mfrc522.uid.uidByte[i]);
+    byte tmp = reader.uid.uidByte[i];
     Serial.print(i);
     Serial.print(" ");
     Serial.println(tmp);
     input[i] = tmp;
   }
+byte master[4] = {0x29, 0xFB, 0x56, 0xD3};
 
-  if(test(input) == true){
+  if(reader.uid.uidByte[0] == master[0] && 
+     reader.uid.uidByte[1] == master[1] &&
+     reader.uid.uidByte[2] == master[2] &&
+     reader.uid.uidByte[3] == master[3]){//checks to see if the input id is the stored user
+    Serial.println("TRUE");
     digitalWrite(7, HIGH);
     digitalWrite(6, LOW);
   }
   else{
+    Serial.println("FALSE");
     digitalWrite(6, HIGH);
     digitalWrite(7, LOW);
   }
