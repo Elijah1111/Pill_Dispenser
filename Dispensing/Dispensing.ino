055b5c0da9a1277beb08c6aab1fa98eb9ac575c3
Elijah <elijahmtcastle@gmail.com>
2019-04-02 20:08:45 -0600

    Display and Chip Reading
    Reads Chips and authenticates then displays a message to the lcd screen, also has a time out

--- Dispensing/Dispensing.ino
+++ Dispensing/Dispensing.ino
@@ -1,24 +1,39 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          10         // Configurable, see typical pin layout above
 
-MFRC522 reader(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 reader(SS_PIN, RST_PIN);   // Create MFRC522 instance
+LiquidCrystal_I2C lcd(0x27, 16, 1); // Create LCD display address, columns, rows
 
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  lcd.init();
+  lcd.backlight();
   SPI.begin();      // Init SPI bus
-  reader.PCD_Init();   // Init MFRC522
+  reader.PCD_Init();   // Init Card Reader
   reader.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  pinMode(7,OUTPUT);
-  pinMode(6,OUTPUT);
+  lcd.print("Please Scan Card");
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
 }
-
+bool known = false, unknown = false;
+short int c = 0;
 void loop() {
   // Look for new cards
+  c++;
+    if(c >= 500){// a time out event
+      c = 0; 
+      unknown = false;
+      known = false;
+      digitalWrite(7, LOW);
+      digitalWrite(6, LOW);
+      lcd.clear();
+      lcd.print("Please Scan Card");
+      return;
+    }
   if ( ! reader.PICC_IsNewCardPresent()) {
     return;
   }
@@ -26,32 +41,37 @@ void loop() {
   if ( ! reader.PICC_ReadCardSerial()) {
     return;
   }
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  //reader.PICC_DumpToSerial(&(reader.uid));
-
-  byte input[4];
-  for(int i =0; i<4; i++){
-    byte tmp = reader.uid.uidByte[i];
-    Serial.print(i);
-    Serial.print(" ");
-    Serial.println(tmp);
-    input[i] = tmp;
-  }
-byte master[4] = {0x29, 0xFB, 0x56, 0xD3};
-
-  if(reader.uid.uidByte[0] == master[0] && 
-     reader.uid.uidByte[1] == master[1] &&
-     reader.uid.uidByte[2] == master[2] &&
-     reader.uid.uidByte[3] == master[3]){//checks to see if the input id is the stored user
-    Serial.println("TRUE");
-    digitalWrite(7, HIGH);
-    digitalWrite(6, LOW);
-  }
-  else{
-    Serial.println("FALSE");
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-  }
-  Serial.println();
   
+  byte master[4] = {0x29, 0xFB, 0x56, 0xD3};//The hexidecimal UID
+
+  if (reader.uid.uidByte[0] == master[0] &&
+      reader.uid.uidByte[1] == master[1] &&
+      reader.uid.uidByte[2] == master[2] &&
+      reader.uid.uidByte[3] == master[3]) {
+    if (!known) { //checks to see if the input id is the stored user
+      Serial.println("TRUE");
+      lcd.clear();
+      lcd.print("WELCOME: Speve");
+      digitalWrite(7, HIGH);
+      digitalWrite(6, LOW);
+      known = true;
+      unknown = false;
+      c=0;
+    }
+  }
+  else if (!unknown) {
+      Serial.println("FALSE");
+      lcd.clear();
+      lcd.print("ACCESS DENIED");
+      digitalWrite(6, HIGH);
+      digitalWrite(7, LOW);
+      unknown = true;
+      known = false;
+      c=0;
+    }
+
+    
+  Serial.println();
+
+
 }
