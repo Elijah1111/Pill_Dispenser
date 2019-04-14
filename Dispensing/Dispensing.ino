7443e18002602a052cc742ea613f4e18d4a222ee
Elijah <elijahmtcastle@gmail.com>
2019-04-14 13:56:30 -0600

    Abstraction
    Abstracted the reader to be a function

--- Dispensing/Dispensing.ino
+++ Dispensing/Dispensing.ino
@@ -8,6 +8,21 @@
 MFRC522 reader(SS_PIN, RST_PIN);   // Create MFRC522 instance
 LiquidCrystal_I2C lcd(0x27, 16, 1); // Create LCD display address, columns, rows
 
+
+bool auth(MFRC522 reader) { //authenticate user
+
+  byte master[4] = {0x29, 0xFB, 0x56, 0xD3};//The hexidecimal UID
+  if (   reader.uid.uidByte[0] == master[0] &&
+         reader.uid.uidByte[1] == master[1] &&
+         reader.uid.uidByte[2] == master[2] &&
+         reader.uid.uidByte[3] == master[3]   ) {
+    return true;
+  }
+  return false;
+
+}
+
+
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
   lcd.init();
@@ -22,56 +37,47 @@ void setup() {
 bool known = false, unknown = false;
 short int c = 0;
 void loop() {
-  // Look for new cards
+  
   c++;
-    if(c >= 500){// a time out event
-      c = 0; 
-      unknown = false;
-      known = false;
-      digitalWrite(7, LOW);
-      digitalWrite(6, LOW);
-      lcd.clear();
-      lcd.print("Please Scan Card");
-      return;
-    }
-  if ( ! reader.PICC_IsNewCardPresent()) {
+  if (c >= 500) { // a time out event
+    c = 0;
+    unknown = false;
+    known = false;
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    lcd.clear();
+    lcd.print("Please Scan Card");
+    return;
+  }
+  if ( ! reader.PICC_IsNewCardPresent()) {// Look for new cards
     return;
   }
   // Select one of the cards
-  if ( ! reader.PICC_ReadCardSerial()) {
+  if ( ! reader.PICC_ReadCardSerial()) {// Cant read
     return;
   }
-  
-  byte master[4] = {0x29, 0xFB, 0x56, 0xD3};//The hexidecimal UID
 
-  if (reader.uid.uidByte[0] == master[0] &&
-      reader.uid.uidByte[1] == master[1] &&
-      reader.uid.uidByte[2] == master[2] &&
-      reader.uid.uidByte[3] == master[3]) {
-    if (!known) { //checks to see if the input id is the stored user
+  if (auth(reader) {
+  if (!known) { //checks to see if the input id is the stored user
       Serial.println("TRUE");
       lcd.clear();
-      lcd.print("WELCOME: Speve");
+      lcd.print("WELCOME: M Bison");
       digitalWrite(7, HIGH);
       digitalWrite(6, LOW);
       known = true;
       unknown = false;
-      c=0;
+      c = 0;
     }
   }
   else if (!unknown) {
-      Serial.println("FALSE");
-      lcd.clear();
-      lcd.print("ACCESS DENIED");
-      digitalWrite(6, HIGH);
-      digitalWrite(7, LOW);
-      unknown = true;
-      known = false;
-      c=0;
-    }
-
-    
-  Serial.println();
-
+  Serial.println("FALSE");
+    lcd.clear();
+    lcd.print("ACCESS DENIED");
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+    unknown = true;
+    known = false;
+    c = 0;
+  }
 
 }
